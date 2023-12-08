0333c20375f9e77beb66a999929ac39f352d97b7
Greg Leach <greg.leach@lairdconnect.com>
2023-12-08 13:29:07 +0000

    feat: PROD-4275 Add RM126x pin names

--- sdk/thirdparty/semtech/SWSD001/smtc_hal/inc/smtc_hal_gpio_pin_names.h
+++ sdk/thirdparty/semtech/SWSD001/smtc_hal/inc/smtc_hal_gpio_pin_names.h
@@ -1,10 +1,11 @@
 /*!
  * @file      smtc_hal_gpio_pin_names.h
  *
- * @brief     Defines NucleoL073 platform pin names
+ * @brief     Defines RM126x platform pin names
  *
  * The Clear BSD License
  * Copyright Semtech Corporation 2021. All rights reserved.
+ * Copyright Laird Connectivity 2023. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted (subject to the limitations in the disclaimer
@@ -59,66 +60,55 @@ extern "C" {
  * --- PUBLIC TYPES ------------------------------------------------------------
  */
 
+#define PA_COUNT 0x9
+#define PB_COUNT 0x5
+#define PC_COUNT 0x8
+#define PD_COUNT 0x4
+
 /*!
  * @brief HAL Gpio pin name
  */
 typedef enum hal_gpio_pin_names_e
 {
     // GPIOA
-    PA_0  = 0x00,
-    PA_1  = 0x01,
-    PA_2  = 0x02,
-    PA_3  = 0x03,
-    PA_4  = 0x04,
-    PA_5  = 0x05,
-    PA_6  = 0x06,
-    PA_7  = 0x07,
-    PA_8  = 0x08,
-    PA_9  = 0x09,
-    PA_10 = 0x0A,
-    PA_11 = 0x0B,
-    PA_12 = 0x0C,
-    PA_13 = 0x0D,
-    PA_14 = 0x0E,
-    PA_15 = 0x0F,
+    PA_0  = 0x0,
+    PA_1  = 0x1,
+    PA_2  = 0x2,
+    PA_3  = 0x3,
+    PA_4  = 0x4,
+    PA_5  = 0x5,
+    PA_6  = 0x6,
+    PA_7  = 0x7,
+    PA_8  = 0x8,
+
     // GPIOB
-    PB_0 = 0x10,
-    PB_1 = 0x11,
-    PB_2 = 0x12,
-    PB_3 = 0x13,
-    PB_4 = 0x14,
-    PB_5 = 0x15,
-    PB_6 = 0x16,
-    PB_7 = 0x17,
-    PB_8 = 0x18,
-    PB_9 = 0x19,
+    PB_0 = 0x09,
+    PB_1 = 0x0A,
+    PB_2 = 0x0B,
+    PB_3 = 0x0C,
+    PB_4 = 0x0D,
+
     // GPIOC
-    PC_0  = 0x20,
-    PC_1  = 0x21,
-    PC_2  = 0x22,
-    PC_3  = 0x23,
-    PC_4  = 0x24,
-    PC_5  = 0x25,
-    PC_6  = 0x26,
-    PC_7  = 0x27,
-    PC_8  = 0x28,
-    PC_9  = 0x29,
-    PC_10 = 0x2A,
-    PC_11 = 0x2B,
-    PC_12 = 0x2C,
-    PC_13 = 0x2D,
-    PC_14 = 0x2E,
-    PC_15 = 0x2F,
-    // GPIOE
-    PE_4 = 0x44,
-    // GPIOH
-    PH_0 = 0x70,
-    PH_1 = 0x71,
-    PH_3 = 0x73,
+    PC_0  = 0x0E,
+    PC_1  = 0x0F,
+    PC_2  = 0x10,
+    PC_3  = 0x11,
+    PC_4  = 0x12,
+    PC_5  = 0x13,
+    PC_6  = 0x14,
+    PC_7  = 0x15,
+
+    // GPIOD
+    PD_0 = 0x16,
+    PD_1 = 0x17,
+    PD_2 = 0x18,
+    PD_3 = 0x19,
+
     // ADC internal channels
     ADC_TEMP = 0xF0,
     ADC_VREF = 0xF1,
     ADC_VBAT = 0xF2,
+
     // Not connected
     NC = -1
 } hal_gpio_pin_names_t;
